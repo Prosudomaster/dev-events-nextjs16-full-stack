a489ec0f984360a7c6d1916047d9c904db8d2967
ProSudoMaster <kgmalik23@gmail.com>
2026-03-14 23:06:47 -0400

    suspens error fixed with adding component

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,147 +1,16 @@
-import { notFound } from "next/navigation";
-import Image from "next/image";
-import BookEvent from "@/components/BookEvent";
-import { getSimilerEventsBySlug } from "@/lib/actions/event.actions";
-import EventCard from "@/components/EventCard";
-import { getPostHogClient } from "@/lib/posthog-server";
-// import { cacheLife } from "next/cache";
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-const EventDetailsItem = ({ icon, alt, label }: { icon: string; alt: string; label: string }) => (
-  <div className="flex-row-gap-2 items-center">
-    <Image src={icon} alt={alt} width={17} height={17} />
-    <span>{label}</span>
-  </div>
-)
-
-const EventAgendaItem = ({ agendaItems}: { agendaItems: string[] }) => (
-  <div className="agenda">
-    <h2>Agenda</h2>
-    <ul>
-      {agendaItems.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
-  </div>
-)
-
-const EventTags = ({ tags }: { tags: string[] }) => (
-  <div className="flex flex-row gap-1.5 flex-wrap">
-    {tags.map((tag) => (
-      <span className='pill' key={tag}>{tag}</span>
-    ))}
-  </div>
-)
+import EventDetails from "@/components/EventDetails"
+import suspensee from "react/suspense"
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  // 'use cache';
-  // cacheLife('hours');
 
-  const { slug } = await params;  
-  const response = await fetch(`${BASE_URL}/api/events/${slug}`, {
-    cache: 'no-store'
-  });
-
-  const { event } = await response.json();
-
-const {
-  title,
-  overview,
-  description,
-  image,
-  date,
-  time,
-  location,
-  mode,
-  agenda,
-  audience,
-  tags,
-  organizer
-} = event;
-
-  if (!description) {
-    return notFound()
-  }
-
-  const posthog = getPostHogClient();
-  posthog.capture({
-    distinctId: "anonymous",
-    event: "event_details_viewed",
-    properties: { slug, title, location, date, mode, tags },
-  });
-  await posthog.shutdown();
-
-  const bookings = 10;
-
-  const similerEvents: Event[] = await getSimilerEventsBySlug(slug);
-
-  return (
-    <section id="event">
-      <div className="header">
-      <h1>{title}</h1>
-      <p>{description}</p>
-      </div>
-
-      <div className="details">
-        <div className="content">
-          <Image src={image} alt="Event-Banner" width={800} height={400} className='banner' />
-
-          <section>
-            <h2>Overview</h2>
-            <p>{overview}</p>
-          </section>
-
-          <section className="flex-col-gap-2">
-            <h2>Event Details</h2>
-             
-             <EventDetailsItem icon="/icons/calendar.svg" alt="calendar" label={date} />
-             <EventDetailsItem icon="/icons/clock.svg" alt="clock" label={time} />
-             <EventDetailsItem icon="/icons/pin.svg" alt="pin" label={location} />
-             <EventDetailsItem icon="/icons/mode.svg" alt="mode" label={mode} />
-             <EventDetailsItem icon="/icons/audience.svg" alt="audience" label={audience} />
-          </section>
-
-    
-          <EventAgendaItem agendaItems={agenda} />
-
-          <section className="flex-col-gap-2">
-            <h2>About the Organizer</h2>
-             <p>{organizer}</p>
-          </section>
-
-          <EventTags tags={tags} />
-
-        </div>
-
-        <aside className="booking">
-          <div className="signup-card">
-
-            <h2>Book Your Spot</h2>
-            
-            {bookings > 0 ? (
-              <p className="text-sm">Join {bookings} people who have already booked their spot!</p>
-            ) : (
-              <p className="text-sm">Be The first to book your form.</p>
-            )}
-
-            <BookEvent eventId={event._id} slug={event.slug} />
-          </div>
-        </aside>
-
-      </div>
-
-            <div className="flex w-full flex-col gap-4 pt-20">
-              <h2>Similar Events</h2>
-              <div className="events">
-                {similerEvents.length > 0 && similerEvents.map((similarEvent) => (
-                  <EventCard key={similarEvent.title} {...similarEvent} />
-                ))}
-              </div>
-            </div>
-
-    </section>
-  )
+  const slug = params.then(p => p.slug);
+ return (
+    <main>
+      <suspense fallback={<div>Loading...</div>}>
+        <EventDetails params={slug} />
+      </suspense>
+    </main>
+ )
 }
 
 export default EventDetailsPage
